756729d3597cbc250582268e097775eb9d3e8468
Daniil Baida <devbaida78@gmail.com>
2025-07-18 16:44:43 +0100

    product routes and controllers

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+import Product from "../models/product.model.js";
+
+export const getProducts = async (req, res, next) => {
+    try {
+        const products = await Product.find({});
+
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getProduct = async (req, res, next) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: true, message: "Inalid ID" });
+    }
+    try {
+        const product = await Product.findById(id);
+
+        if (!product) {
+            const error = new Error("Product not found");
+            error.status = 404;
+            throw error;
+        }
+        res.status(200).json({ success: true, data: product });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const createProduct = async (req, res, next) => {
+    const product = req.body;
+
+    if (!product.name || !product.price || !product.image) {
+        const error = new Error("Please provide all fields");
+        error.status(400);
+        throw error;
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateProduct = async (req, res, next) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: true, message: "Inalid ID" });
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+            new: true,
+        });
+
+        if (!updatedProduct) {
+            const error = new Error("Product not found");
+            error.status = 404;
+            throw error;
+        }
+
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteProduct = async (req, res, next) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: true, message: "Inalid ID" });
+    }
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(id);
+
+        if (!deletedProduct) {
+            const error = new Error("Product not found");
+            error.status = 404;
+            throw error;
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Product deleted successfully",
+        });
+    } catch (error) {
+        next(error);
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+
+import errorMiddleware from "./middlewares/error.middleware.js";
+import productRouter from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(errorMiddleware);
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body;
+app.use("/api/products", productRouter);
 
-    if (!product.name || !product.price || !product.image) {
-        return res
-            .status(400)
-            .json({ success: false, message: "Please provide all fields" });
-    }
+const PORT = process.env.PORT || 5000;
 
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-        console.error("Error in Create product:", error.message);
-        res.status(500).json({ success: false, message: "Server Error" });
-    }
-});
-
-app.listen(5000, () => {
-    connectDB();
-    console.log("Server started at http://localhost:5000");
-});
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running at http://localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("❌ Failed to connect to MongoDB:", err);
+        process.exit(1);
+    });
